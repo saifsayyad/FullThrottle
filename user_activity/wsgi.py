3f2a97bde29199dd3198a0c3be02c0889c27e9e6
Saifali.Saiyyad <Saifali.Saiyyad@partner.bmw.de>
2020-07-06 17:15:39 +0530

    Updating requirements.txt

--- user_activity/wsgi.py
+++ user_activity/wsgi.py
@@ -1,8 +1,7 @@
 import os
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = Cling(get_wsgi_application())
