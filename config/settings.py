73867083efc2277f512704d4194c85a2777ea078
Saifali.Saiyyad <Saifali.Saiyyad@partner.bmw.de>
2020-07-06 15:53:55 +0530

    Resolving STATIC_URL error

--- config/settings.py
+++ config/settings.py
@@ -112,6 +112,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = 'user_activity/static'
+STATIC_URL = 'user_activity/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'user_activity', 'static')
